487254b89a785bf2e4488a9b94d423366431bb84
Fern-Frog <lataforg@gmail.com>
2024-10-17 11:45:48 -0400

    First upload of P3LAB

---
+++ P3LAB_SchwabAlta.py
@@ -0,0 +1,24 @@
+# Alta Schwab
+# 10/17/2024
+# P3LAB
+# Calculate coin combibnations for given value
+
+'''
+# Regular Divison
+print(100/3)
+
+# Floor Division
+print(100//3)
+
+# Modulus Division
+print(100%3)
+print(7%4)
+'''
+
+# Get money value from the user
+money = float(input("Enter the amount of money as a float: $"))
+
+# Convert money to a whole number
+money = int(round(money * 100))
+
+print(money)
